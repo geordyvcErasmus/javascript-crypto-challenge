9849ca2a26239517dfed6578149001bb9a7946b3
geordy van cutsem <geordy.van.cutsem@student.ehb.be>
2019-08-21 10:46:58 +0200

    Fixed the problem with libsodium.

--- src/Signature.js
+++ src/Signature.js
@@ -1,15 +1,24 @@
-const tweetnacl = require('tweetnacl');
+//const tweetnacl = require('tweetnacl');
+const nacl = require('libsodium-wrappers');
 // had to use tweetnacl because libsodium-wrappers is broke.
 // sodium.crypto_sign_keypair() would not work as it wasn't recognised.
-let keypair = tweetnacl.sign.keyPair();
+let keypair = null;
+let loadLibsodium = async () => await nacl.ready;
+
+(async () => {
+    await loadLibsodium();
+    keypair = nacl.crypto_sign_keypair();
+})();
 
 async function verifyingKey(){
-    console.log(keypair);
+    //console.log(keypair);
+    await loadLibsodium();
     return keypair.publicKey;
 }
 
 async function sign(msg){
-    return tweetnacl.sign(msg, keypair.secretKey);
+    await loadLibsodium();
+    return nacl.crypto_sign(msg, keypair.privateKey);
 }
 
 module.exports.verifyingKey = verifyingKey;
--- tests/sign.test.js
+++ tests/sign.test.js
@@ -1,19 +1,18 @@
 const nacl = require('libsodium-wrappers');
-const tweetnacl = require('tweetnacl');
 const Signature = require('../src/Signature');
 
 describe('signing module', () => {
   it('provides a verifying key', async () => {
     expect(await Signature.verifyingKey()).toBeDefined()
-  })
+  });
   it('returns a signed message', async () => {
-    const msg = tweetnacl.randomBytes(1024);
+    //const msg = tweetnacl.randomBytes(1024);
     // commented function won't work cause libsodium-wrappers...
-    // const msg = nacl.randombytes_buf(1024);
+    const msg = nacl.randombytes_buf(1024);
     const signedMsg = await Signature.sign(msg);
     const verifyingKey = await Signature.verifyingKey();
     // same reason as previous function
-    // expect(nacl.crypto_sign_open(signedMsg, verifyingKey)).toEqual(msg)
-    expect(tweetnacl.sign.open(signedMsg, verifyingKey)).toEqual(msg)
+    expect(nacl.crypto_sign_open(signedMsg, verifyingKey)).toEqual(msg)
+    //expect(tweetnacl.sign.open(signedMsg, verifyingKey)).toEqual(msg)
   })
-})
+});
